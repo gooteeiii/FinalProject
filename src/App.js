f6d8dd83fe800561b28791053b4ceec20c6eb6d9
gooteeiii <90635663+gooteeiii@users.noreply.github.com>
2021-11-30 22:38:01 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import './App.css';
+
+const App = () => {
+   const [name, setName] = useState('');
+   const [pwd, setPwd] = useState('');
+   const [cookies, setCookie] = useCookies(['user']);
+
+   const handle = () => {
+      setCookie('Name', name, { path: '/' });
+      setCookie('Password', pwd, { path: '/' });
+   };
+   return (
+      <div className="App">
+      <h1>Name of the user:</h1>
+      <input
+         placeholder="name"
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+      />
+      <h1>Password of the user:</h1>
+      <input
+         type="password"
+         placeholder="name"
+         value={pwd}
+         onChange={(e) => setPwd(e.target.value)}
+      />
+      <div>
+         <button onClick={handle}>Set Cookie</button>
+      </div>
+      <br />
+      {cookies.Name && (
+      <div>
+         Name: <p>{cookies.Name}</p>
+      </div>
+      )}
+      {cookies.Password && (
+      <div>
+         Password: <p>{cookies.Password}</p>
+      </div>
+      )}
+   </div>
+   );
+};
+export default App;
